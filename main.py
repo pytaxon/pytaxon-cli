c829e68f200689bae9b382d9083f6db03e5b24b0
Ninniet5670 <marconeto3000@gmail.com>
2023-10-30 01:24:33 -0300

    test

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from pytaxon import Pytaxon
 if __name__ == '__main__':
     pt = Pytaxon()
     print(pt.logo)
-    pt.read_spreadshet(input('Digite o caminho do seu arquivo: '))
+    # pt.read_spreadshet(input('Digite o caminho do seu arquivo: '))
+    pt.read_spreadshet("F:/0 - Bibliotecas Windows/Área de trabalho/pytaxon/pytaxon-cli/Lepidoptera_-_Importacao_IX_lote_1.xls")
 
     while True:
         match input(pt.menu):
             case '1':
-                pt.read_taxon_columns(*input('Digite o nome das colunas do Genero e da Especie: ').split())
-                pt.connect_to_api()
+                # pt.read_taxon_columns(*input('Digite o nome das colunas do Genero e da Especie: ').split())
+                pt.read_taxon_columns(*['Genus1', 'Species1'])
+                pt.connect_to_api_taxony()
                 pt.data_incorrect_taxons()
                 pt.create_taxonomies_pivot_spreadsheet()
 
@@ -17,7 +19,9 @@ if __name__ == '__main__':
                 pt.update_original_spreadsheet()
 
             case '3':
-                pt.read_lineage_columns(*input('Digite o nome das colunas da Tribo, Família, Ordem, Classe e Filo: ').split())
+                # pt.read_lineage_columns(*input('Digite o nome das colunas da Tribo, Família, Ordem, Classe e Filo: ').split())
+                pt.read_lineage_columns(*['Tribe1', 'Family', 'Order', 'Class', 'Phylum'])
+                pt.connect_to_api_lineage()
 
             case '4':
                 pass
