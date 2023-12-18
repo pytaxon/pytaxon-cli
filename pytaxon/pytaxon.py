8768e04b83d74789351b42a213eb20bce41a4a84
Ninniet5670 <marconeto3000@gmail.com>
2023-12-18 12:11:04 -0300

    argparse added for cli interaction

--- pytaxon/pytaxon.py
+++ pytaxon/pytaxon.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-from pytaxon import Pytaxon_GBIF
+import argparse
 
-def main():
-    pt = Pytaxon_GBIF()
-    if len(sys.argv) == 2:
-        if not pt.connect_to_api:
-            print('Could not connect to GBIF API')
-            exit()
+from pytaxon import Pytaxon_GBIF, Pytaxon_OTT
 
-        print(pt.logo)
-        pt.read_spreadshet(sys.argv[1])
+
+def main():  # sourcery skip: extract-method
+    parser = argparse.ArgumentParser(description='Pytaxon baby')
+    parser.add_argument('--gbif', help='Use the GBIF service API')
+    parser.add_argument('--ott', help='Use the Open Tree of Life service API')
+    parser.add_argument('--od', '--originalData', help='Use the Open Tree of Life service API')
+    parser.add_argument('--cd', '--correctedData', help='Use the Open Tree of Life service API')
+    args = parser.parse_args()
+
+    if args.gbif:
+        pt = Pytaxon_GBIF()
+        pt.read_spreadshet(args.gbif)
         pt.read_columns()
         pt.check_species_and_lineage()
+
+    elif args.ott:
+        pt = Pytaxon_OTT()
+        print(pt.logo)
+        if not pt.connect_to_api:
+            print('Could not connect to OpenTree API')
+            exit()
+
+        pt.read_spreadshet(args.ott)
+        pt.read_taxon_columns()
+        pt.connect_to_api_taxony()
+        pt.data_incorrect_taxons()
+        pt.create_taxonomies_pivot_spreadsheet()
+
+    elif args.od and args.cd:
+        pt = Pytaxon_GBIF()
+        pt.update_original_spreadsheet(args.od, args.cd)
     else:
-        print('Please pass the path to the spreadsheet')
+        print('Error')
+
 
 if __name__ == '__main__':
     main()
