f21a176da72500aec4dfa6d3d5759f5e857c836f
PROENÇA, Marco <54338596+Ninniet5670@users.noreply.github.com>
2024-02-17 16:22:57 -0300

    setuptools required; v5.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as arq:
 
 setup(
     name='pytaxon',
-    version='5.0.8',
+    version='5.0.9',
     setup_requires=['wheel'],
     license='MIT License',
     author='Marco Aurélio Proença Neto, Marcos Paulo Alves de Sousa',
@@ -27,6 +27,6 @@ setup(
     },
     install_requires=['pandas', 'openpyxl', 'tqdm', 'xlrd', 'requests', 
                       'argparse', 'Jinja2', 'ttkthemes', 'tk', 'matplotlib', 
-                      'customtkinter', 'CTkMessagebox'],
+                      'customtkinter', 'CTkMessagebox', 'setuptools],
     url='https://github.com/pytaxon'
 )
