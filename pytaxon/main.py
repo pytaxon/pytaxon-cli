70fd18e4daae7df1789ce6304297039d9758b068
Ninniet5670 <marconeto3000@gmail.com>
2024-04-18 06:31:31 -0300

    better help messages

--- pytaxon/main.py
+++ pytaxon/main.py
@@ -4,15 +4,15 @@ from pytaxon import Pytaxon
 
 
 def main(): 
-    parser = argparse.ArgumentParser(description='Pytaxon baby')
+    parser = argparse.ArgumentParser(description='Pytaxon arguments to use on terminal')
     parser.add_argument('-i', '--input', help='Input of original spreadsheet')
     parser.add_argument('-r', '--columns', help="Input of column names: 'Kingdom, Phylum, Class, Order, Family, Genus, Species, ScientificName'")
     parser.add_argument('-c', '--to_check', type=str, help='Name of spreadsheet to check')
     parser.add_argument('-si', '--source_id', type=int, help='Number of the source ID')
 
-    parser.add_argument('-os', '--original_spreadsheet', help='Use the Open Tree of Life service API')
-    parser.add_argument('-cs', '--checked_spreadsheet', help='Use the Open Tree of Life service API')
-    parser.add_argument('-o', '--output', help='Name of checked spreadsheet')
+    parser.add_argument('-os', '--original_spreadsheet', help='Path of original spreadsheet for updating')
+    parser.add_argument('-cs', '--checked_spreadsheet', help='Path of checked spreadsheet for updating')
+    parser.add_argument('-o', '--output', help='Name of output spreadsheet')
     args = parser.parse_args()
 
     if args.original_spreadsheet and args.checked_spreadsheet:
