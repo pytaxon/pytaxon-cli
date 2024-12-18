a29abe8a08169632334e35bc70d0c56c6bf8b097
Ninniet5670 <marconeto3000@gmail.com>
2024-12-18 17:17:28 -0300

    🎨 feat: Refactored main parameters

--- pytaxon/main.py
+++ pytaxon/main.py
@@ -5,25 +5,22 @@ from pytaxon import Pytaxon
 
 def main(): 
     parser = argparse.ArgumentParser(description='Pytaxon arguments to use on terminal')
-    parser.add_argument('-i', '--input', help='Input of original spreadsheet')
+    parser.add_argument('-os', '--original_spreadsheet', type=str, help='Path of original spreadsheet')
+    parser.add_argument('-ss', '--suggestion_spreadsheet', type=str, help='Path of suggestion spreadsheet')
     parser.add_argument('-r', '--columns', help="Input of column names: 'Kingdom, Phylum, Class, Order, Family, Genus, Species, ScientificName'")
-    parser.add_argument('-c', '--to_check', type=str, help='Name of spreadsheet to check')
     parser.add_argument('-si', '--source_id', type=int, help='Number of the source ID')
-
-    parser.add_argument('-os', '--original_spreadsheet', help='Path of original spreadsheet for updating')
-    parser.add_argument('-cs', '--checked_spreadsheet', help='Path of checked spreadsheet for updating')
-    parser.add_argument('-o', '--output', help='Name of output spreadsheet')
+    parser.add_argument('-c', '--corrected', type=str, help='Name of corrected spreadsheet')
     args = parser.parse_args()
 
-    if args.original_spreadsheet and args.checked_spreadsheet:
+    if not args.columns and args.source_id:
         pt = Pytaxon()
-        pt.update_original_spreadsheet(args.original_spreadsheet, args.checked_spreadsheet, args.output)
+        pt.update_original_spreadsheet(args.original_spreadsheet, args.suggestion_spreadsheet, args.corrected)
     else:
         pt = Pytaxon(args.source_id)
-        pt.read_spreadshet(args.input)
+        pt.read_spreadshet(args.original_spreadsheet)
         pt.read_columns(args.columns)
         pt.check_species_and_lineage()
-        pt.create_to_correct_spreadsheet(args.to_check)
+        pt.create_to_correct_spreadsheet(args.suggestion_spreadsheet)
         
 
 if __name__ == '__main__':
