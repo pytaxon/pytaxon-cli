867ff80d75592704d3ee5464058c29a0431a7e48
Ninniet5670 <marconeto3000@gmail.com>
2024-12-19 17:33:48 -0300

    🐛 fix: Changed API parameter

--- pytaxon/pytaxon.py
+++ pytaxon/pytaxon.py
@@ -114,7 +114,7 @@ class Pytaxon:
             "withAllMatches": False,
             "withCapitalization": False,
             "withSpeciesGroup": False,
-            "withUninomialFuzzyMatch": False,
+            "withUninomialFuzzyMatch": True,
             "withStats": True,
             "mainTaxonThreshold": 0.6
             }
